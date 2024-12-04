b0d4618064f10fb93be734ebc6af8f415524e07e
Prathamesh Lakare <prathameshlakare001@gmail.com>
2024-12-04 17:35:05 +0530

    feat: added dueDate into model

--- api/models/task.model.js
+++ api/models/task.model.js
@@ -12,20 +12,39 @@ const taskSchema = new mongoose.Schema({
     { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   ],
   tags: [{ type: String }],
-  timeToComplete: { type: Number, required: true },
   status: {
     type: String,
     enum: ["To Do", "In Progress", "Completed", "Blocked"],
     default: "To Do",
   },
+  timeToComplete: { type: Number, required: true, default: 1 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  dueDate: { type: Date },
 });
 
 // Automatically update the `updatedAt` field whenever the document is updated
 taskSchema.pre("save", function (next) {
   this.updatedAt = Date.now();
+
+  if (this.timeToComplete) {
+    const dueDate = new Date(this.createdAt);
+    dueDate.setDate(dueDate.getDate() + this.timeToComplete);
+    this.dueDate = dueDate;
+  }
+
   next();
 });
 
 module.exports = mongoose.model("Task", taskSchema);
+
+// taskSchema.pre("findOneAndUpdate", function (next) {
+//   const update = this.getUpdate();
+//   if (update.timeToComplete) {
+//     const dueDate = new Date(update.createdAt || Date.now());
+//     dueDate.setDate(dueDate.getDate() + update.timeToComplete);
+//     update.dueDate = dueDate;
+//   }
+//   update.updatedAt = Date.now();
+//   next();
+// });
